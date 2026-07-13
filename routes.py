22cbbb7e16c780e39a56cbe4cd0c898628027151
vivopermatasari@gmail.com <email@kamu.com>
2026-07-13 15:28:11 +0700

    Langkah 4: Buat halaman publik About dan Portfolio → commit

--- routes.py
+++ routes.py
@@ -9,3 +9,7 @@ def index():
 @main.route('/about')
 def about():
     return render_template('about.html')
+
+@main.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
