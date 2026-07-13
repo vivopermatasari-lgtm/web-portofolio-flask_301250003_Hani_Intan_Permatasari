f6e39868bfefe6ed2504a38eccaf15b2bbd7c185
vivopermatasari@gmail.com <email@kamu.com>
2026-07-13 15:26:25 +0700

    Langkah 4: Buat halaman About (templates/about.html) + route /about di routes.py → commit

--- routes.py
+++ routes.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/about')
+def about():
+    return render_template('about.html')
